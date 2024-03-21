aeab091074a8005cf7d90060330d1978474cec64
A.Galdos <agaldos@lksnext.com>
2024-03-21 17:06:41 +0100

    adding resources

--- views/repositories.py
+++ views/repositories.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, session, Blueprint
+from flask import Flask, render_template, session, Blueprint, request, redirect
 from model.repository import Repository
+from model.resource import Resource
+
+from extensions import db
+
 
 repositories_blueprint = Blueprint('repositories', __name__)
 
@@ -17,3 +21,37 @@ def repository(app_id, repository_id):
 def repository_playground(app_id, repository_id):
     repo = Repository.query.filter_by(repository_id=repository_id).first()
     return render_template('repositories/playground.html', app_id=app_id, repo=repo)
+
+@repositories_blueprint.route('/app/<app_id>/repository/<repository_id>/settings', methods=['GET'])
+def repository_settings(app_id, repository_id):
+    repo = Repository.query.filter_by(repository_id=repository_id).first()
+    return render_template('repositories/settings.html', app_id=app_id, repo=repo)
+
+@repositories_blueprint.route('/app/<app_id>/repository/<repository_id>/resource/<resource_id>/delete', methods=['GET'])
+def resource_delete(app_id, repository_id, resource_id):
+    Resource.query.filter_by(resource_id=resource_id).delete()
+    db.session.commit()
+    return repository(app_id, repository_id)
+
+@repositories_blueprint.route('/app/<app_id>/repository/<repository_id>/resource', methods=['POST'])
+def resource_create(app_id, repository_id):
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser submits an empty file with no filename
+        if file.filename == '':
+            return redirect(request.url)
+        if file : #and allowed_file(file.filename):
+            
+            resource = Resource(name=request.form['name'], uri=file.filename, repository_id=repository_id)
+            
+            db.session.add(resource)
+            db.session.commit()
+
+        return repository(app_id, repository_id)
+
+
+
+
+
