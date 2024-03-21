19d76fb09e33866af80da51206d620e2d039a87b
A.Galdos <agaldos@lksnext.com>
2024-03-21 15:41:13 +0100

    repo menu

--- views/repositories.py
+++ views/repositories.py
@@ -12,3 +12,8 @@ def repositories(app_id):
 def repository(app_id, repository_id):
     repo = Repository.query.filter_by(repository_id=repository_id).first()
     return render_template('repositories/repository.html', app_id=app_id, repo=repo)
+
+@repositories_blueprint.route('/app/<app_id>/repository/<repository_id>/playground', methods=['GET'])
+def repository_playground(app_id, repository_id):
+    repo = Repository.query.filter_by(repository_id=repository_id).first()
+    return render_template('repositories/playground.html', app_id=app_id, repo=repo)
