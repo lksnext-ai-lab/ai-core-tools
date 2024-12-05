04600bd6af9acc8f84c6299f0a161c01d7320b10
Mikel Lonbide Yudego <mlonbide@lksnext.com>
2024-12-05 17:11:15 +0100

    Update model descriptions in alembic migration to use consistent casing for 'gpt-4o'.

--- alembic/versions/a460ad150e9e_add_initial_models.py
+++ alembic/versions/a460ad150e9e_add_initial_models.py
@@ -26,7 +26,7 @@ def upgrade():
         ),
         [
             {'provider': 'OpenAI', 'name': 'gpt-4o-mini', 'description': 'PT-4o mini (“o” for “omni”) is our most advanced model in the small models category, and our cheapest model yet. It is multimodal (accepting text or image inputs and outputting text), has higher intelligence than gpt-3.5-turbo but is just as fast. It is meant to be used for smaller tasks, including vision tasks.'},
-            {'provider': 'OpenAI', 'name': 'GPT-4o', 'description': 'GPT-4o: Our high-intelligence flagship model for complex, multi-step tasks. GPT-4o is cheaper and faster than GPT-4 Turbo. Currently points to gpt-4o-2024-08-06.'}
+            {'provider': 'OpenAI', 'name': 'gpt-4o', 'description': 'gpt-4o: Our high-intelligence flagship model for complex, multi-step tasks. gpt-4o is cheaper and faster than gpt-4 Turbo. Currently points to gpt-4o-2024-08-06.'}
         ]
     )
 
